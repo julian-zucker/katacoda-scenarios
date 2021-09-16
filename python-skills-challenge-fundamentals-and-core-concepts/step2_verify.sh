c7cc86113e76537311d8d4c7ee2147738b383775
Julian Zucker <julian.zucker@protonmail.com>
2021-09-16 16:55:19 -0400

    Remove assets

--- python-skills-challenge-fundamentals-and-core-concepts/step2_verify.sh
+++ python-skills-challenge-fundamentals-and-core-concepts/step2_verify.sh
@@ -1,1 +1,1 @@
-python -c "import assets.step2; assert assets.step2.get_table('abcbcb') == {'a': {'b': 1}, 'b': {'c': 2}, 'c': {'b': 2}}"
+python -c "import step2; assert step2.get_table('abcbcb') == {'a': {'b': 1}, 'b': {'c': 2}, 'c': {'b': 2}}"
