47010e66aea9a48aa87479aa97166a929a6521b2
Julian Zucker <julian.zucker@protonmail.com>
2021-12-21 07:07:41 -0800

    Use length

--- python-skills-challenge-write-file-functions/step1_verify.sh
+++ python-skills-challenge-write-file-functions/step1_verify.sh
@@ -1,1 +1,1 @@
-python3 -c "import step1; step1.fetch_url('http://example.com', 'example.txt')" && wc -l example.txt > /dev/null
+python3 -c "import step1; step1.fetch_url('http://example.com', 'example.txt'); assert len(open('example.txt').readlines())"
